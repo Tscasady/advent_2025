263334a88a46e0010017721179f424b55327d587
tsc <tscasady@gmail.com>
2025-12-29 13:46:58 -0600

    Day2 part1.

--- day2/day2.rb
+++ day2/day2.rb
@@ -1,17 +1,35 @@
-input = File.read("./day2/day2_test.txt")
+def split(n)
+
+  digits = Math.log10(n).to_i + 1
+
+  return if digits.odd?
+
+  divisor = 10**(digits / 2)
+
+  first_half = n / divisor
+  second_half = n % divisor
+
+  [first_half, second_half]
+end
+
+input = File.read("./day2/day2_input.txt")
 
 result = []
+
+
 input.split(",") do |range|
   start, finish = range.split("-")
+  start = start.to_i
+  finish = finish.to_i
   range = Range.new(start, finish)
   range.each { |num|
-    length = num.length
-    next if length.odd?
-    first = num[0..length/2 - 1]
-    rest = num[length/2..]
+    first, rest = split(num)
+    next unless first
     result << num if first == rest
   }
 end
+
+
 
 puts result.flatten.sum(&:to_i)
 
